128b4f8884caef50687a62f50c81d1e9f4c12cfd
Iker Etxebarria <ietxebi@gmail.com>
2023-09-21 18:22:11 +0200

    Etxekalte 1.0 - Primer commit
    Se ha creado el proyecto con spring boot initializer

---
+++ src/main/java/eus/etxekalte/EtxekalteWebApplication.java
@@ -0,0 +1,13 @@
+package eus.etxekalte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtxekalteWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtxekalteWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eus/etxekalte/EtxekalteWebApplicationTests.java
@@ -0,0 +1,13 @@
+package eus.etxekalte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtxekalteWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
